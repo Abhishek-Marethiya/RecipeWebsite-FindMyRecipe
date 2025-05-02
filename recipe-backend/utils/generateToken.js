d499f08ddfc8303281c8aee00e6e65cf5ad584a0
Abhishek-Marethiya <168818112+Abhishek-Marethiya@users.noreply.github.com>
2025-05-02 21:49:23 +0530

    Update generateToken.js

--- recipe-backend/utils/generateToken.js
+++ recipe-backend/utils/generateToken.js
@@ -6,7 +6,7 @@ const jwt=require('jsonwebtoken')
 	res.cookie('jwt', token, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === 'production',
-		sameSite: 'strict',
+		sameSite: 'None',
 		maxAge: 7 * 24 * 60 * 60 * 1000,
 	});
 };
